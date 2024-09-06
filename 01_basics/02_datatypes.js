f2cc46afc1a1430fff1d4447cb68c445b7ef0c55
Sanjeev kumar <75210303+Sanjeevkr12212@users.noreply.github.com>
2024-09-06 12:05:25 +0000

    Data Types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser so run here
+
+console.log(3
+  +  
+3) // code readability should be high
+
+console.log("Sanjeev")
+
+
+let name ="Sanjeev"
+let age = 18
+let isLoggedIn= false
+
+//Primitive Data Type
+
+// number => 2 to power 53
+// bight
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+//Non Primitive dataType
+
+// object
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); object
+
+
